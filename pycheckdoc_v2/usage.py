ec92bf20d5f06250cb532c7c6d02b98ee85c4494
Brian Kimathi <bryo.kim1@gmail.com>
2023-10-08 22:15:04 +0300

    Updated the usage string

--- pycheckdoc_v2/usage.py
+++ pycheckdoc_v2/usage.py
@@ -1,5 +1,38 @@
 """pycheckdoc_v2 usage"""
 
 
+USAGE = """
+Path to the file/directory to check can either be relative
+or absolute.
+
+Path is relative if it's a direct child of the cwd.
+
+If path to a directory is provided, any .py files existing
+in the directory are checked.
+
+Both relative and absolute paths can be passed as arguments
+for the same call.
+    pycheckdoc file.py /home/user/dev/main.py
+
+Examples:
+    Check file.py that is in the cwd:
+        pycheckdoc file.py
+
+    Check file.py in the src directory in the cwd:
+        pycheckdoc src/file.py
+
+    Check all .py files in the src directory in the cwd:
+        pycheckdoc src
+        pycheckdoc src/*.py
+
+    Check a file or directory not in the cwd:
+        pycheckdoc /home/user/dev/file.py   -> File
+        pycheckdoc /home/user/dev   -> Directory
+"""
+
+
 def print_usage():
-    print("USAGE: pycheckdoc [filename]")
+    """Print usage"""
+    print("USAGE: pycheckdoc [path]")
+
+    print(USAGE)
