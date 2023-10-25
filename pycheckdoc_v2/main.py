4623d6b09acd9f190a5f924b1b84f498b9d0b0a7
Brian Kimathi <bryo.kim1@gmail.com>
2023-10-25 20:04:07 +0300

    Add new flag to toggle error and success message printing.

--- pycheckdoc_v2/main.py
+++ pycheckdoc_v2/main.py
@@ -3,6 +3,7 @@
 
 import sys
 import argparse
+from typing import List, Tuple
 
 # Local
 from pycheckdoc_v2.generate_ast import get_ast
@@ -26,18 +27,37 @@ parser.add_argument(
 )
 
 parser.add_argument(
+    "--no-print",
+    dest="print",
+    action="store_false",
+    help="Don't print error or success messages",
+)
+
+parser.add_argument(
     "paths", nargs="*", help="Paths to files/directories to check"
 )
-cli_args = parser.parse_args()
+args = parser.parse_args()
 
 
-def main(args: argparse.Namespace = cli_args):
+def main(
+    paths: List[str] = args.paths,
+    recursive: bool = args.recursive,
+    print_msgs: bool = args.print,
+) -> Tuple[int, int]:
     """Pycheckdoc entry point.
 
+    Arguments are read from the command line if none is provided.
+
     Args:
-        `args` (Namespace, optional): Arguments passed to the programme in a
-            `argparse.Namespace` . Defaults to `cli_args`. `cli_args` are
-            arguments provided in the command line.
+        paths (List[str], optional): List of the paths to check.
+        recursive (Bool, optional): Whether to check directories recursively.
+            Defaults to `False`.
+        print_msgs (Bool, optional): Whether to print file errors and success
+            messages. Defaults to `True`.
+
+    Returns:
+        Tuple[int, int]: Tuple of total errors and files with errors. If an
+            error occurs then (-1, -1) is returned.
 
     Usage
     ---
@@ -53,15 +73,15 @@ def main(args: argparse.Namespace = cli_args):
     ```
     """
 
-    if len(args.paths) == 0:
+    if len(paths) == 0:
         print_usage()
         sys.exit(1)
 
-    modules = get_ast(args.paths, recursive=args.recursive)
+    modules = get_ast(paths, recursive=recursive)
 
     if modules is None:  # Files provided don't exist
         print("Files provided don't exist")
-        return
+        return (-1, -1)
 
     total_errors = 0
     files_with_errors = 0
@@ -69,9 +89,9 @@ def main(args: argparse.Namespace = cli_args):
     for module in modules:
         prev_count = total_errors
 
-        module_errors = check_module_doc(module)
-        func_errors = check_function_doc(module)
-        class_errors, method_errors = check_class_doc(module)
+        module_errors = check_module_doc(module, print_msgs)
+        func_errors = check_function_doc(module, print_msgs)
+        class_errors, method_errors = check_class_doc(module, print_msgs)
 
         total_errors += (
             module_errors + func_errors + class_errors + method_errors
@@ -80,10 +100,44 @@ def main(args: argparse.Namespace = cli_args):
         if prev_count < total_errors:
             files_with_errors += 1
 
-    if total_errors != 0 and files_with_errors != 0:
-        print_error(total_errors, files_with_errors, len(modules))
-    else:
-        print_success(len(modules))
+    if print_msgs:
+        if total_errors != 0 and files_with_errors != 0:
+            print_error(total_errors, files_with_errors, len(modules))
+        else:
+            print_success(len(modules))
+
+    return (total_errors, files_with_errors)
+
+
+def check_doc(
+    paths: List[str],
+    recursive: bool = False,
+    print_msgs: bool = False,
+) -> Tuple[int, int]:
+    """Check python file documentation.
+    Use this in other python files instead of main.
+
+    Args:
+        paths (List[str], optional): List of the paths to check.
+        recursive (Bool, optional): Whether to check directories recursively.
+            Defaults to `False`.
+        print_msgs (Bool, optional): Whether to print file errors and success
+            messages. Defaults to `False`.
+
+    Raises:
+        ValueError: If length of paths is 0.
+
+    Returns:
+        Tuple[int, int]: Tuple of total errors and files with errors.
+    """
+    if len(paths) == 0:
+        raise ValueError
+
+    return main(
+        paths=paths,
+        recursive=recursive,
+        print_msgs=print_msgs,
+    )
 
 
 if __name__ == "__main__":
